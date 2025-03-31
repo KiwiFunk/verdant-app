efa415afcfafab58d671de0db8c8ee919946c419
Alex <alex_p97@live.com>
2025-03-31 22:12:39 +0100

    Finished logic for modal open/close in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,34 @@ import Groups from './components/Groups'
 import './App.css'
 
 function App() {
+
+  // Manage states for modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [groupId, setGroupId] = useState(null);
+
+  // Functions for handling modal state
+  const handleOpenModal = (id) => {
+    setGroupId(id);
+    setIsModalOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setGroupId(null);
+    setIsModalOpen(false);
+  }
+
   return (
     <div id="app-container">
       <Groups onAddPlant={handleOpenModal} />
+
+      {/* Handle Plant Creation Modal */}
+      {isModalOpen && (
+        <CreatePlantModal
+          groupId={groupId}
+          onOpen={isModalOpen}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
   )
 }
