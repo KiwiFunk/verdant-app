a8ededa15146319e092cd4e70013670eb9f5b5e9
Alex <alex_p97@live.com>
2025-04-02 00:09:22 +0100

    Removed logic for calculating water level. Will rework it.

--- server/src/models/PlantModel.js
+++ server/src/models/PlantModel.js
@@ -13,19 +13,4 @@ const PlantSchema = new mongoose.Schema({
     position: { type: Number }
 });
 
-// Calulate water percentage based on lastWatered and waterFrequency
-PlantSchema.methods.calculateWaterLevel = function() {
-    if (!this.lastWatered) return 0;
-
-    const now = new Date();
-    const lastWatered = new Date(this.lastWatered);
-    // Date in JS uses ms by default. 1000ms = 1s, 60s = 1m, 60m = 1h
-    const elapsedHours = (now - lastWatered) / (1000 * 60 * 60);
-    const dailyRate = 100 / this.waterFrequency;
-    //Daily rate divided by hours in a day gives us the hourly rate.
-    const hourlyRate = dailyRate / 24;
-    
-    return Math.max(0, Math.round(100 - (elapsedHours * hourlyRate)));
-};
-
 module.exports = mongoose.model('Plant', PlantSchema);
--- server/src/routes/plantRoutes.js
+++ server/src/routes/plantRoutes.js
@@ -5,6 +5,5 @@ const plantController = require('../controllers/plantController');
 router.post('/', plantController.createPlant);
 router.delete('/:id', plantController.deletePlant);
 router.patch('/water/:id', plantController.waterPlant);         // Water the target plant
-router.get('/water-level/:id', plantController.getWaterLevel);  // Get the water level of a plant
 
 module.exports = router;
