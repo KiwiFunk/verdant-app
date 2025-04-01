af03780c8586d6d0aff804d8cff02ed2055546ef
Alex <alex_p97@live.com>
2025-04-01 01:24:00 +0100

    Created card routes

---
+++ server/src/routes/cardRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');                                 // Import express for Router
+const router = express.Router();                                    // Create a new Router
+const cardController = require('../controllers/cardController');    // Import cardController
+
+router.get('/', cardController.getAllCards);                        // Get all cards
+router.post('/', cardController.createCard);                        // Create a new card
+router.patch('/:id', cardController.updateCard);                    // Update a card by ID
+router.delete('/:id', cardController.deleteCard);                   // Delete a card by ID
+
+module.exports = router;                                            // Export the router for use in the app
