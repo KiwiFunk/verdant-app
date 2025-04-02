9a7bf32f1a5c385815e5bf23d6c547c152431527
Alex <alex_p97@live.com>
2025-04-02 21:32:21 +0100

    Added isCollapsed bool to Group model to store state

--- server/src/models/GroupModel.js
+++ server/src/models/GroupModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const GroupSchema = new mongoose.Schema({
-    name: { type: String, required: true },                            // Group Name
-    plants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Plant' }]   // Array of plant IDs
+    name: { type: String, required: true },                             // Group Name
+    plants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Plant' }],   // Array of plant IDs
+    isCollapsed: { type: Boolean, default: false }                      // Collapse state
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
