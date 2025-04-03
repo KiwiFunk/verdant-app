5ff4e0df96765bcbd2e4d3527d28464f1819e27c
Alex <alex_p97@live.com>
2025-04-03 14:52:58 +0100

    Added break in header to seperate user settings from main nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,10 @@
 function Header() {
     return (
         <header>
-            <div className='branding'>
+            <div className='header-main'>
                 <i className="bi bi-flower3"></i>
                 <h1>Verdant</h1>
             </div>
-            <div className='bar-fill'></div>
             <div className='user-controls'>
                 <div className='user-icon'>
                     <i className="bi bi-person-circle"></i>
