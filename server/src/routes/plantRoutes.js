7fde0f58369f63761759155b46541a740f9171ff
Alex <alex_p97@live.com>
2025-04-03 19:12:00 +0100

    Fixed typo in route name

--- server/src/routes/plantRoutes.js
+++ server/src/routes/plantRoutes.js
@@ -4,7 +4,7 @@ const plantController = require('../controllers/plantController');
 
 router.post('/', plantController.createPlant);
 router.delete('/:id', plantController.deletePlant);
-router.patch('/water/:id', plantController.updatePlant);         // Water the target plant
+router.patch('/water/:id', plantController.waterPlant);         // Water the target plant
 router.patch('/:id', plantController.updatePlant);               // Update the target plant
 
 module.exports = router;
