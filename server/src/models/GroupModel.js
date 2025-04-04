61e8b4434930fcf0d81ca5e14694e2f561dcc8d8
Alex <alex_p97@live.com>
2025-04-05 00:33:42 +0100

    Updated position field in group schema

--- server/src/models/GroupModel.js
+++ server/src/models/GroupModel.js
@@ -4,7 +4,7 @@ const GroupSchema = new mongoose.Schema({
     name: { type: String, required: true },                             // Group Name
     plants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Plant' }],   // Array of plant IDs
     isCollapsed: { type: Boolean, default: false },                     // Collapse state
-    position: { type: Number }                                          // Position of the group
+    position: { type: Number, default: 0, index: true }                 // Position of the group
 });
 
 module.exports = mongoose.model('Group', GroupSchema);
